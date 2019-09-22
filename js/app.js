8d842432aa83aebe2d7602983a6af959db5b60e5
fangqingyi <719041306@qq.com>
2019-09-22 11:02:43 +0800

    class Enemy

--- js/app.js
+++ js/app.js
@@ -1,28 +1,33 @@
 // Enemies our player must avoid
-//  定义一个一般的游戏元素GameElement
+//  定义一般的类：游戏元素GameElement,在屏幕上显示enemy，用render的方法
 class GameElement {
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
 
-var Enemy = function() {
-    // 这是一个应用到每个实例的类-enemy
-    
+// 这是一个应用到每个实例的类-enemy
+class Enemy extends GameElement {
+    constructor(enemyNumber){
     // enemy的图片/sprite已经用一个帮助程序来加载了
     this.sprite = 'images/enemy-bug.png';
-};
+    this.x = Math.random()*300 + 5;
+    this.y = enemyNumber === 1 ? 55 : (enemyNumber === 2 ? 140 : 255);
+    this.speed = 0;
+    }
 
 // 更新enemy的位置，用一个方法，参数dt，作为时间增量
-Enemy.prototype.update = function(dt) {
+    update(dt) {
+        if (this.x >= 500 || this.speed === 0) {
+            this.speed = Math.random()*3 + 2;
+            this.x = -200;
+        } else {
     // 所有的运动都应乘以参数dt，来保证游戏运动速度
-    
+            this.x = Math.floor(Math.random()*dt + this.speed) + this.x;
+        }
+    }
 };
 
-// 在屏幕上显示enemy，用game的方法
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
 
 // 定义一个player类，具有： update(), render() ，handleInput()方法
 var Player = function() {
